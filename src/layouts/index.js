1693394d5c7fbc41704e1f74f8e68a30c69b6581
Ramin B <ramin@wework.com>
2017-07-17 12:02:02 -0400

    redux FTW

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import { connect, Provider } from 'react-redux';
 import Helmet from 'react-helmet';
 
 import Link from 'gatsby-link';
-import { Provider } from 'rebass';
+import { Provider as ThemeProvider } from 'rebass';
 
+import createStore from '../state/createStore';
 import Header from './header';
 
 import '../css/global.scss';
@@ -18,30 +19,32 @@ export default class Template extends React.Component {
 
   render() {
     return (
-      <div>
-        <Helmet
-          title="Candid Co"
-          meta={[
-            { name: "description", content: "Sample" },
-            { name: "keywords", content: "sample, something" },
-          ]}
-        />
-        <Provider
-          theme={{
-            font: '"Source Sans Pro", "Helvetica Neue", Helvetica, Arial, sans-serif',
-            fontSizes: [
-              12, 16, 24, 36, 48, 72
-            ],
-            space: [ 0, 6, 12, 18, 24 ],
-            breakpoints: [ 32, 48, 64, 80, 90 ]
-          }}
-        >
-          <Header />
+      <Provider store={createStore()}>
+        <div>
+          <Helmet
+            title="Candid Co"
+            meta={[
+              { name: "description", content: "Sample" },
+              { name: "keywords", content: "sample, something" },
+            ]}
+          />
+          <ThemeProvider
+            theme={{
+              font: '"Source Sans Pro", "Helvetica Neue", Helvetica, Arial, sans-serif',
+              fontSizes: [
+                12, 16, 24, 36, 48, 72
+              ],
+              space: [ 0, 6, 12, 18, 24 ],
+              breakpoints: [ 32, 48, 64, 80, 90 ]
+            }}
+          >
+            <Header />
 
-          { this.props.children() }
+            { this.props.children() }
 
-        </Provider>
-      </div>
+          </ThemeProvider>
+        </div>
+      </Provider>
     )
   }
 }
