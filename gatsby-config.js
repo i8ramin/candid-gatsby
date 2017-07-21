f97d62600ae38ad2dd1a858789e09e9ee79c0558
i8ramin <i8ramin@gmail.com>
2017-07-21 08:44:56 -0400

    adding cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,11 +8,15 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-sass`,
+    `gatsby-plugin-nprogress`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-transformer-remark`,
     {
-      resolve: `gatsby-plugin-nprogress`,
-    },
-    {
-      resolve: `gatsby-plugin-sitemap`
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `mizmzkx6bjh6`,
+        accessToken: process.env.CONTENTFUL_API_TOKEN,
+      },
     },
   ],
 }
